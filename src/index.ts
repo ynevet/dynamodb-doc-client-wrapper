c29f675785f80d53bdb6f890463abe666f2b70e7
Yair Nevet <ynevet@chegg.com>
2020-09-04 18:46:06 +0300

    init repo & commit

---
+++ src/index.ts
@@ -0,0 +1,59 @@
+import AWS from 'aws-sdk';
+import AmazonDaxClient from 'amazon-dax-client';
+import { DocumentClient } from 'aws-sdk/lib/dynamodb/document_client';
+
+export class DocumentClientWrapper {
+    endpoints: (string | undefined)[];
+    region: string;
+    clientType: ClientType;
+    timeout: number;
+
+    constructor(clientType: ClientType, endpoints: (string | undefined)[], region: string, timeout: number) {
+
+        if (!endpoints || endpoints.length === 0) {
+            throw new Error("endpoints can't be null or empty");
+        }
+
+        if (!region) {
+            throw new Error("region can't be null");
+        }
+
+        this.endpoints = endpoints;
+        this.region = region;
+        this.clientType = clientType;
+        this.timeout = timeout
+    }
+
+    getClient(): DocumentClient {
+        let service: AWS.DynamoDB | undefined;
+
+        switch (this.clientType) {
+            case ClientType.DynamoDB: {
+                const [endpoint] = this.endpoints;
+                service = new AWS.DynamoDB({
+                    region: this.region,
+                    endpoint: endpoint,
+                    httpOptions: {
+                        timeout: this.timeout,
+                    },
+                    maxRetries: 3
+                });
+                break;
+            }
+            case ClientType.DynamoDBDax: {
+                service = new AmazonDaxClient({ endpoints: this.endpoints, region: this.region });
+                break;
+            }
+        }
+
+        return new AWS.DynamoDB.DocumentClient({
+            service: service,
+            region: this.region,
+        });
+    }
+}
+
+export enum ClientType {
+    DynamoDB,
+    DynamoDBDax
+}
